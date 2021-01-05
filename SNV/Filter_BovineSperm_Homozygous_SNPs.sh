02d33abe824bc432f1b0c33a556c875056699758
Roel Janssen <roel@gnu.org>
2021-01-05 16:26:31 +0100

    Remove inline Guix usage.
    * SNV/Filter_BovineSperm_Homozygous_SNPs.sh: Remove inline Guix usage.

--- SNV/Filter_BovineSperm_Homozygous_SNPs.sh
+++ SNV/Filter_BovineSperm_Homozygous_SNPs.sh
@@ -6,40 +6,46 @@
 
 # This script first merges the temporary vcfs generated for each chromosome by "Genotype_SNPs_Parallel.R" into one VCF. This VCF contains the sperm SNP info about the positions heterozygous in the embryos.
 echo "# Merging VCFs"
-echo "bcftools concat $1*.vcf.gz > $2BovineSperm_SNPs_Het_Embryo.vcf"
-guixr load-profile ~/.guix-profile/ -- <<EOF
-	bcftools concat $1*.vcf.gz -O z -o $2BovineSperm_SNPs_Het_Embryo.vcf.gz
-	bcftools index $2BovineSperm_SNPs_Het_Embryo.vcf.gz
-	bcftools stats $2BovineSperm_SNPs_Het_Embryo.vcf.gz > $2BovineSperm_SNPs_Het_Embryo.stats
 
-	echo "# Select high quality SNV calls"
+COMMAND="bcftools concat $1*.vcf.gz -O z -o $2BovineSperm_SNPs_Het_Embryo.vcf.gz"
+echo "$COMMAND"
+eval "$COMMAND"
 
-	# First remove SNPs next to indels
-	bcftools norm -d all $2BovineSperm_SNPs_Het_Embryo.vcf.gz -O z -o $2Deduplicated_positions.vcf.gz
-	bcftools index $2Deduplicated_positions.vcf.gz
+COMMAND="bcftools index $2BovineSperm_SNPs_Het_Embryo.vcf.gz"
+echo "$COMMAND"
+eval "$COMMAND"
 
-	# select the positions that have to be removed
-	bcftools isec -C $2BovineSperm_SNPs_Het_Embryo.vcf.gz $2Deduplicated_positions.vcf.gz -O z -o $2Duplicated_positions.vcf.gz -w1
+COMMAND="bcftools stats $2BovineSperm_SNPs_Het_Embryo.vcf.gz > $2BovineSperm_SNPs_Het_Embryo.stats"
+echo "$COMMAND"
+eval "$COMMAND"
 
-	bcftools view -T ^$2Duplicated_positions.vcf.gz $2BovineSperm_SNPs_Het_Embryo.vcf.gz -O z -o $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz
-	bcftools index $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz
+echo "# Select high quality SNV calls"
 
-	# Remove variant calls with a depth lower than 10 or higher than 75. Also remove indels
-	bcftools view -e "DP>75 | DP<10 | IDV>0" -V indels $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz -o $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf
-	bgzip -c $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf > $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz
-	bcftools index $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz
-	bcftools stats $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz > $2BovineSperm_SNPs_Het_Embryo_Filtered2.stats
-	#	
-	echo "# Selecting homozygous SNPs"
-	bcftools view -g hom -V indels $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz > $2BovineSperm_Hom_SNPs.vcf
-	
-	echo "# Zip and index vcfs"
-	bgzip -c $2BovineSperm_Hom_SNPs.vcf > $2BovineSperm_Hom_SNPs.vcf.gz
-	bcftools index $2BovineSperm_Hom_SNPs.vcf.gz
-	bcftools stats $2BovineSperm_Hom_SNPs.vcf.gz > $2BovineSperm_Hom_SNPs.stats
-	echo "# Zip and index vcfs"
-	
-EOF
+# First remove SNPs next to indels
+bcftools norm -d all $2BovineSperm_SNPs_Het_Embryo.vcf.gz -O z -o $2Deduplicated_positions.vcf.gz
+bcftools index $2Deduplicated_positions.vcf.gz
+
+# select the positions that have to be removed
+bcftools isec -C $2BovineSperm_SNPs_Het_Embryo.vcf.gz $2Deduplicated_positions.vcf.gz -O z -o $2Duplicated_positions.vcf.gz -w1
+
+bcftools view -T ^$2Duplicated_positions.vcf.gz $2BovineSperm_SNPs_Het_Embryo.vcf.gz -O z -o $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz
+bcftools index $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz
+
+# Remove variant calls with a depth lower than 10 or higher than 75. Also remove indels
+bcftools view -e "DP>75 | DP<10 | IDV>0" -V indels $2BovineSperm_SNPs_Het_Embryo_Filtered.vcf.gz -o $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf
+bgzip -c $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf > $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz
+bcftools index $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz
+bcftools stats $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz > $2BovineSperm_SNPs_Het_Embryo_Filtered2.stats
+
+echo "# Selecting homozygous SNPs"
+bcftools view -g hom -V indels $2BovineSperm_SNPs_Het_Embryo_Filtered2.vcf.gz > $2BovineSperm_Hom_SNPs.vcf
+
+echo "# Zip and index vcfs"
+bgzip -c $2BovineSperm_Hom_SNPs.vcf > $2BovineSperm_Hom_SNPs.vcf.gz
+bcftools index $2BovineSperm_Hom_SNPs.vcf.gz
+bcftools stats $2BovineSperm_Hom_SNPs.vcf.gz > $2BovineSperm_Hom_SNPs.stats
+echo "# Zip and index vcfs"
+
 
 # Write the homozygous position to a bed file (chr start end GT (=10th column)
 echo "# Select homozygous SNP positions with paternal genotype"
@@ -66,14 +72,11 @@ echo "# Sort, zip and index SNP positions file. Remove duplicated lines."
 sort -k1,1 -k2,2n $2"Embryo_Het_SNP_Positions.txt" | uniq -u > $2"Embryo_Het_SNP_Positions_Sorted.txt"
 
 # zip and index the SNP position files
-guixr load-profile ~/.guix-profile/ -- <<EOF
-	bgzip $2"BovineSperm_Hom_Positions_Sorted.txt"
-	tabix -s1 -b2 -e3 $2"BovineSperm_Hom_Positions_Sorted.txt.gz"
+bgzip $2"BovineSperm_Hom_Positions_Sorted.txt"
+tabix -s1 -b2 -e3 $2"BovineSperm_Hom_Positions_Sorted.txt.gz"
 
-	bgzip -c $2"Embryo_Het_SNP_Positions_Sorted.txt" > $2"Embryo_Het_SNP_Positions_Sorted.txt.gz"
-	tabix -s1 -b2 -e3 $2"Embryo_Het_SNP_Positions_Sorted.txt.gz"
-
-EOF
+bgzip -c $2"Embryo_Het_SNP_Positions_Sorted.txt" > $2"Embryo_Het_SNP_Positions_Sorted.txt.gz"
+tabix -s1 -b2 -e3 $2"Embryo_Het_SNP_Positions_Sorted.txt.gz"
 
 echo "Finished"
 
